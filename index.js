3c8331c8762fde23999cc1a434ddc135dd60250f
saumitraebits <saumitra@ealphabits.com>
2024-08-12 22:35:55 +0530

    Node rtsp streaming server using rtsp-relay library

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const { proxy, scriptUrl } = require("rtsp-relay")(app);
+
+const handler = proxy({
+  url: `rtsp://user:Admin$123@103.162.197.92:600/media/video2`,
+  // if your RTSP stream need credentials, include them in the URL as above
+  verbose: true,
+  transport: "tcp",
+});
+
+// the endpoint our RTSP uses
+app.ws("/api/stream", handler);
+
+// this is an example html page to view the stream
+app.get("/", (req, res) =>
+  res.send(`
+  <canvas id='canvas'></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`)
+);
+
+app.listen(2000);
